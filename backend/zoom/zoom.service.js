d762f960ac1503fdca683086df156e15a159ba12
Nkachukwu Victor <nkachukwuvictor2016@gmail.com>
2025-07-14 20:26:57 +0100

    new commit

--- backend/zoom/zoom.service.js
+++ backend/zoom/zoom.service.js
@@ -62,13 +62,51 @@ const generateZoomMeeting = async () => {
       },
       body: JSON.stringify({
         topic: "Test Meeting",
-        type: 1, // Instant meeting
+        agenda: "My Zoom test Meeting",
+        default_password: false,
+        start_time: new Date().toLocaleDateString(), // Use current time for instant meeting
+        timezone: "Africa/Lagos",
+        duration: 60,
+        password: "12345",
+        type: 1, // 1, Instant meeting, 2, Scheduled meeting
         settings: {
           host_video: true,
-          participant_video: true,
-          join_before_host: true,
-          mute_upon_entry: false,
+          participant_video: true, // Allow participant video
+          join_before_host: false, // Don't Allow participants to join before the host
+          mute_upon_entry: true, // Mute participants upon entry
           waiting_room: false,
+          allow_multiple_devices: true,
+          breakout_room: {
+            enable: false,
+            rooms: [
+              {
+                name: "Breakout Room 1",
+                participants: ["nkachukwuvictor2020@gmail.com", "John"],
+              },
+            ],
+          },
+          calendar_type: 1, // 1 for Google Calendar, 2 for Outlook Calendar
+          contact_email: "nkachukwuvictor2016@gmail.com",
+          contact_name: "Victor Nwachukwu",
+          email_notification: true,
+          encryption_type: "enhanced_encryption", // Use 'enhanced_encryption' for better security
+          auto_recording: "none", // Options: 'none', 'local', 'cloud'
+          focus_mode: true, // Focus mode allows participants to focus on the host's video
+
+          meeting_authentication: true,
+          meeting_invitees: [
+            {
+              email: "nkachukwuvictor2020@gmail.com",
+            },
+          ],
+
+          private_meeting: true, // Make the meeting private
+          watermark: false, // Disable watermark
+          continuous_meeting_chat: {
+            enable: true,
+            auto_add_invited_external_users: true,
+            auto_add_meeting_participants: true,
+          },
         },
       }),
     });
@@ -85,4 +123,4 @@ const generateZoomMeeting = async () => {
   }
 };
 
-generateZoomAccessToken();
+generateZoomMeeting();
